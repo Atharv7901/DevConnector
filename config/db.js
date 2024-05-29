914d92b2c52a2dbe0e149f1374039603ab37ac4b
Atharv Shivarkar <atharvshivarkar@Atharvs-MacBook-Air.local>
2024-05-30 01:51:44 +0530

    Mongodb connection

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db);
+    console.log("Connected to mongodb...");
+  } catch (err) {
+    console.error(err);
+    //EXITS THE CODE
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
