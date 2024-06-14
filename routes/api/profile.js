1967c07f8718e84866f52d2e7baca3d480327338
Atharv Shivarkar <atharvshivarkar@gmail.com>
2024-06-14 23:11:00 +0530

    Added experience put route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -163,4 +163,40 @@ router.delete("/", auth, async (req, res) => {
   }
 });
 
+//@route PUT  api/profile/experience
+//@desc  Add profile experience
+//@access Private
+router.put(
+  "/experience",
+  [
+    auth,
+    [
+      check("title", "title is required").not().isEmpty(),
+      check("company", "company is required").not().isEmpty(),
+      check("from", "From date is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json(400).json({errors: errors.array()});
+    }
+
+    const {title, company, location, from, to, current, description} = req.body;
+    const newExp = {title, company, location, from, to, current, description};
+
+    try {
+      const profile = await Profile.findOne({user: req.user.id});
+
+      profile.experience.unshift(newExp);
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.json(500).send("Internal server error");
+    }
+  }
+);
+
 module.exports = router;
