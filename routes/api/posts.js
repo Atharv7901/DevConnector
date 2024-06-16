2753721a81d27b4b7809cc83d17716ce3ad6dc47
Atharv Shivarkar <atharvshivarkar@gmail.com>
2024-06-17 01:00:35 +0530

    Unlike post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -126,4 +126,34 @@ router.put("/like/:id", auth, async (req, res) => {
   }
 });
 
+//@route PUT /api/posts/unlike/:id
+//@desc  Unlike a post
+//@access Private
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //check if post has been liked yet or not
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({msg: "Post has not yet been liked!"});
+    }
+
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex);
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Internal server error!");
+  }
+});
+
 module.exports = router;
