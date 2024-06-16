a5332571abd5ae02d1e2c67c58867ae8f8e0eaa2
Atharv Shivarkar <atharvshivarkar@gmail.com>
2024-06-17 02:17:15 +0530

    Last BE- Delete comment route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -189,4 +189,39 @@ router.post(
   }
 );
 
+//@route DELETE /api/posts/comment/:id/:comment_id
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //take the comment
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    //comment exists
+    if (!comment) {
+      return res.status(404).json({msg: "Comment not found!"});
+    }
+
+    //check user permissions
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({msg: "Unauthorized to delete comment!"});
+    }
+
+    const removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Internal server error");
+  }
+});
+
 module.exports = router;
