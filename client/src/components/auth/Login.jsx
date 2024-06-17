d2e26b9ba643dd0e612a4e5956fc4b354341a7d2
Atharv Shivarkar <atharvshivarkar@gmail.com>
2024-06-18 01:03:28 +0530

    LogIn FE

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,8 +1,57 @@
+import {Fragment, useState} from "react";
+import {Link} from "react-router-dom";
+
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const {email, password} = formData;
+
+  const handleFormDataChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value});
+  };
+
+  const onLogin = (e) => {
+    e.preventDefault();
+    console.log("Logged in user", formData);
+  };
+
   return (
-    <div>
-      <h1> Login</h1>
-    </div>
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i>
+        Sign In to your account
+      </p>
+      <form className="form" onSubmit={(e) => onLogin(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => handleFormDataChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={(e) => handleFormDataChange(e)}
+            minLength={6}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Create Account</Link>
+      </p>
+    </Fragment>
   );
 };
 
