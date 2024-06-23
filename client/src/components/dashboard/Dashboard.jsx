d65b8299269ebd5346e4b18754a675c82d23e933
Atharv Shivarkar <atharvshivarkar@gmail.com>
2024-06-24 00:56:40 +0530

    Added profile logic

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,4 +1,23 @@
+import {useDispatch} from "react-redux";
+import {useGetLoggedInProfileQuery} from "../../services/profile/profileService";
+import {useEffect} from "react";
+import {getProfile, profileError} from "../../features/profile/profile";
+
 const Dashboard = () => {
+  const userData = useGetLoggedInProfileQuery({});
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    userData.refetch();
+  }, []);
+
+  useEffect(() => {
+    if (userData.isSuccess) {
+      dispatch(getProfile(userData.data));
+    } else if (userData.isError) {
+      dispatch(profileError(userData.error.data));
+    }
+  }, [userData]);
   return (
     <>
       <div>Dashboard</div>
