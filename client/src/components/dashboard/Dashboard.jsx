b3f94eadb855924f6f1d7c9bc2712917b404f4d8
Atharv Shivarkar <atharvshivarkar@gmail.com>
2024-06-25 03:04:18 +0530

    Profile changes

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,27 +1,55 @@
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {useGetLoggedInProfileQuery} from "../../services/profile/profileService";
-import {useEffect} from "react";
+import {Fragment, useEffect, useState} from "react";
 import {getProfile, profileError} from "../../features/profile/profile";
+import Spinner from "../layout/Spinner";
+import {Link} from "react-router-dom";
 
 const Dashboard = () => {
-  const userData = useGetLoggedInProfileQuery({});
+  const [skipUser, setSkipUser] = useState(true);
+  const userData = useGetLoggedInProfileQuery({}, {skip: skipUser});
   const dispatch = useDispatch();
 
+  //redux states
+  const profile = useSelector((state) => state.profile.profile);
+  const loading = useSelector((state) => state.profile.loading);
+  const user = useSelector((state) => state.auth.user);
+
   useEffect(() => {
-    userData.refetch();
+    setSkipUser(false);
   }, []);
 
   useEffect(() => {
-    if (userData.isSuccess) {
-      dispatch(getProfile(userData.data));
-    } else if (userData.isError) {
-      dispatch(profileError(userData.error.data));
+    if (!skipUser) {
+      userData.refetch().then((result) => {
+        if (result.isSuccess) {
+          dispatch(getProfile(result.data));
+        } else if (result.isError) {
+          dispatch(profileError(result.error.data));
+        }
+      });
     }
-  }, [userData]);
-  return (
-    <>
-      <div>Dashboard</div>
-    </>
+  }, [skipUser]);
+
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+        <Fragment>Has</Fragment>
+      ) : (
+        <Fragment>
+          <p>You have not setup a profile yet, please add some info</p>
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Create Profile
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
   );
 };
 
