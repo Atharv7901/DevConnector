d2e7cecca5c85e3aea3d644f815d2eb284e84625
Atharv Shivarkar <atharvshivarkar@gmail.com>
2024-07-01 03:35:46 +0530

    Like post implementation

--- client/src/components/posts/PostItem.jsx
+++ client/src/components/posts/PostItem.jsx
@@ -1,9 +1,62 @@
 import Moment from "react-moment";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {Link} from "react-router-dom";
+import {
+  useAddLikeMutation,
+  useRemoveLikesMutation,
+} from "../../services/post/postService";
+import {useEffect} from "react";
+import {updateLikes} from "../../features/post/post";
+import {setAlert} from "../../features/alerts/alert";
 
 const PostItem = (props) => {
   const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const [addLike, responseLikePost] = useAddLikeMutation();
+  const [removeLike, responseRemoveLike] = useRemoveLikesMutation();
+
+  useEffect(() => {
+    if (responseLikePost.isSuccess) {
+      dispatch(
+        updateLikes({
+          id: responseLikePost.data._id,
+          likes: responseLikePost.data,
+        })
+      );
+      dispatch(setAlert({msg: "Liked Post!", alertType: "success"}));
+    } else if (responseLikePost.isError) {
+      dispatch(
+        setAlert({msg: responseLikePost.error.data.msg, alertType: "danger"})
+      );
+      console.log("errpr", responseLikePost);
+    }
+  }, [responseLikePost]);
+
+  const likePost = (e) => {
+    e.preventDefault();
+    addLike({postID: props.post._id});
+  };
+
+  useEffect(() => {
+    if (responseRemoveLike.isSuccess) {
+      dispatch(
+        updateLikes({
+          id: responseRemoveLike.data._id,
+          likes: responseRemoveLike.data,
+        })
+      );
+      dispatch(setAlert({msg: "Disliked post!", alertType: "success"}));
+    } else if (responseRemoveLike.isError) {
+      dispatch(
+        setAlert({msg: responseRemoveLike.error.data.msg, alertType: "danger"})
+      );
+    }
+  }, [responseRemoveLike]);
+
+  const handleDislike = (e) => {
+    e.preventDefault();
+    removeLike({postID: props.post._id});
+  };
   return (
     <div className="post bg-white p-1 my-1">
       <div>
@@ -16,7 +69,11 @@ const PostItem = (props) => {
         <p className="post-date">
           Posted on <Moment format="YYYY/MM/DD">{props.post.date}</Moment>
         </p>
-        <button type="button" className="btn btn-light">
+        <button
+          type="button"
+          className="btn btn-light"
+          onClick={(e) => likePost(e)}
+        >
           <i className="fas fa-thumbs-up" />{" "}
           <span>
             {props.post.likes.length > 0 && (
@@ -24,7 +81,11 @@ const PostItem = (props) => {
             )}
           </span>
         </button>
-        <button type="button" className="btn btn-light">
+        <button
+          onClick={(e) => handleDislike(e)}
+          type="button"
+          className="btn btn-light"
+        >
           <i className="fas fa-thumbs-down" />
         </button>
         <Link to={`/post/${props.post._id}`} className="btn btn-primary">
