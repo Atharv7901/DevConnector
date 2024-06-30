9449e04e175132346d7fa6ce7ef3b928ee9b31da
Atharv Shivarkar <atharvshivarkar@gmail.com>
2024-07-01 02:15:46 +0530

    Post store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,16 +1,20 @@
 import {combineReducers} from "redux";
 import alertReducer from "../features/alerts/alert";
 import authReducer from "../features/auth/auth";
+import postReducer from "../features/post/post";
 import {authApi} from "../services/auth/authService";
 import profileReducer from "../features/profile/profile";
 import {profileApi} from "../services/profile/profileService";
+import {postApi} from "../services/post/postService";
 
 const rootReducer = combineReducers({
   alerts: alertReducer,
   auth: authReducer,
   profile: profileReducer,
+  post: postReducer,
   [authApi.reducerPath]: authApi.reducer,
   [profileApi.reducerPath]: profileApi.reducer,
+  [postApi.reducerPath]: postApi.reducer,
 });
 
 export default rootReducer;
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -16,6 +16,7 @@ import storage from "redux-persist/lib/storage";
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import rootReducer from "./index";
 import {profileApi} from "../services/profile/profileService";
+import {postApi} from "../services/post/postService";
 
 //persist config
 const persistConfig = {
@@ -37,7 +38,8 @@ const store = configureStore({
       },
     })
       .concat(authApi.middleware)
-      .concat(profileApi.middleware);
+      .concat(profileApi.middleware)
+      .concat(postApi.middleware);
   },
 });
 
