135fa0888f0e19a8dd078f6256e5dedbd43600b8
Atharv Shivarkar <atharvshivarkar@gmail.com>
2024-07-05 01:50:10 +0530

    build ready

--- server.js
+++ server.js
@@ -2,26 +2,34 @@ const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
 
+const path = require("path");
+import {fileURLToPath} from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 //connects to DB
 connectDB();
 
 //use cors
-app.use(cors())
+app.use(cors());
 
 //Init the middleware
 app.use(express.json({extended: false}));
-
-app.get("/", (req, res) => {
-  res.send("Api is running");
-});
 
 //Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
+
+//client
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+});
 
 const PORT = process.env.PORT || 8000;
 
